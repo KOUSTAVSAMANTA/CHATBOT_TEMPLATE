e16a410ed1b0111548a1e6755b481ed7c89b20cd
KOUSTAVSAMANTA <koustavsamanta007@gmail.com>
2023-04-12 19:01:10 +0530

    v2

--- src/components/chatbot.jsx
+++ src/components/chatbot.jsx
@@ -3,7 +3,7 @@ import { ChatContext } from '../Context/Chatcontext';
 import { Widget,addResponseMessage,setQuickButtons,addUserMessage } from "react-chat-widget";
 import 'react-chat-widget/lib/styles.css';
 import axios from 'axios';
-
+import Dropzone from "react-dropzone";
 
 console.log("hello")
 // const data = {
@@ -21,8 +21,22 @@ const api = {"key":"http://127.0.0.1:8000/",'ft':true}
 
 
 function Chatbot() {
+
+  const [files, setFiles] = useState([]);
+
+  const handleFileUpload = (acceptedFiles) => {
+    // Send the file to the server using an HTTP request or WebSocket connection
+    // Display the file in the chat widget
+    addResponseMessage(`File uploaded: ${acceptedFiles[0].name}`);
+  };
+  const handleDrop = (acceptedFiles) => {
+    setFiles(acceptedFiles);
+    handleFileUpload(acceptedFiles);
+  };
+
     const [data, setData] = useState([]);
-    const [history,setHistory] = useState([]);
+    // const [history,setHistory] = useState([['HI how may i help you. \n can i know your current city name?']]);
+    const [history,setHistory] = useState([['I am AdviserrBot, your virtual assistant at BDM Insurance. I am her to assist you with all your policy related queries and guide you through our process. \n \n Please let me know what are you looking for?']]);
     const [opt, setOpt] = useState([]);
     const {dispatch} = useContext(ChatContext);
     // const {data} = useContext(ChatContext);
@@ -46,27 +60,46 @@ function Chatbot() {
     }
       console.log(data)
     const handleQuickButtonClicked = (message)=> {
-      addUserMessage (message);
-      // data.push(message) // DATA STORAGE
-      axios.post( api["key"]+'chatbot',{'msg':{"msg":message,"hst":history,"options":opt}})
-        .then(res=>{
-          console.log(res.data)
-          setHistory(res.data['hist'])
-          setOpt(res.data['opt'])
-          data.push(res.data['msg']) // DATA STORAGE
-          addResponseMessage(res.data['msg'])
-          if (res['options'] !==[] ){
-            setQuickButtons(res.data['options'])
-          }
-          else{
-            setQuickButtons([]);
-          }
-        })
-      .catch(err=>{
-        console.log(err);
-      });
+      if (message.toLowerCase() !=="claim status")
+      {
+        
+        addUserMessage (message);
+        // data.push(message) // DATA STORAGE
+        axios.post( api["key"]+'chatbot',{'msg':{"msg":message,"hst":history,"options":opt}})
+          .then(res=>{
+            console.log(res.data)
+            setHistory(res.data['hist'])
+            setOpt(res.data['opt'])
+            data.push(res.data['msg']) // DATA STORAGE
+            addResponseMessage(res.data['msg'])
+            if (res.data['options'] !==[] ){
+              setQuickButtons(res.data['options'])
+            }
+            else{
+              setQuickButtons([]);
+            }
+          })
+        .catch(err=>{
+          console.log(err);
+        });
+      }
+      else{
+        files.length > 0 && (
+          <Dropzone onDrop={handleDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag and drop a file here, or click to select a file</p>
+              </div>
+            )}
+          </Dropzone>
+        )
+      }
     }
     const handleNewUserMessage = (newMessage) => {
+        console.log({'msg':{"msg":newMessage,"hst":history,"options":opt} })
+        
+        console.log("his:-",history)
         // data.push(newMessage) // DATA STORAGE
         axios.post( api["key"]+'chatbot',{'msg':{"msg":newMessage,"hst":history,"options":opt} })
         .then(res=>{
@@ -75,7 +108,7 @@ function Chatbot() {
           setOpt(res.data['opt'])
           data.push(res.data['msg']) // DATA STORAGE
           addResponseMessage(res.data['msg'])
-          if (res['options'] !==[] ){
+          if (res.data['options'] !==[] ){
             setQuickButtons(res.data['options'])
           }
         })
@@ -85,7 +118,6 @@ function Chatbot() {
         console.log(data)
       };
       console.log("options:-",opt)
-      console.log("his:-",history)
       // console.log("options:-",opt)
 
 
@@ -100,8 +132,9 @@ function Chatbot() {
         autofocus={true}
         handleQuickButtonClicked={handleQuickButtonClicked}
         showTimeStamp={true}
-        
       />
+        
+      
   );
 
 }
